4969e0b9c1a9525a511b00127a8e48dd9ec46a15
Adam <adamwmcneil@gmail.com>
2024-07-20 19:50:58 -0500

    made controller no get requests yet

---
+++ src/lib/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let text: string;
+    export let onClick = () => (console.log("hello world"));
+    export let key: string = "";
+</script>
+
+{#if key == "small"}	
+<button class="small-button shadow" on:click={onClick}> 
+    {text}
+</button>
+{:else if key == "arrow"}
+    <button class="arrow shadow" on:click={onClick}> 
+        {text}
+    </button>
+{:else}
+    <button class="button shadow" on:click={onClick}> 
+        {text}
+    </button>
+{/if}
+
+<style>
+	@import '../app.css';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import {
+    onMount
+} from 'svelte';
+import Button from "$lib/Button.svelte";
+import InputField from '$lib/InputField.svelte';
+let game_state: string;
+let ip: string;
+let valid_states: Set<string> = new Set<string>(["join", "controller"]);
+
+onMount(() => {
+    if (!localStorage.getItem('game_state')) {
+        localStorage.setItem('game_state', 'join');
+        game_state = 'join';
+    } else {
+        if (valid_states.has(localStorage.getItem('game_state'))) {
+            game_state = localStorage.getItem('game_state');
+        } else {
+            localStorage.setItem('game_state', 'join');
+            game_state = 'join';
+        }
+    }
+});
+
+function onJoin() {
+    game_state = "controller"
+    localStorage.setItem('game_state', game_state);
+
+}
+
+function onLeave() {
+    game_state = "join"
+    localStorage.setItem('game_state', game_state);
+
+}
+</script>
+
+<main>
+    {#if game_state == "join"}
+    <div>
+	    <InputField bind:value={ip} text="enter the ip address" />
+    </div>
+    <div>
+        <Button text="Join" onClick={onJoin} key = ""/>
+    </div>
+    {:else}
+    <div>
+        <Button text="<" onClick={() => console.log("hello")} key = "arrow"/>
+        <Button text=">" onClick={() => console.log("hello")} key = "arrow"/>
+        <Button text="jump" onClick={() => console.log("hello")} key = ""/>
+        <Button text="dive" onClick={() => console.log("hello")} key = ""/>
+    </div>
+    <div class="bottomright">
+        <Button text="<" onClick={onLeave} key = ""/>
+    </div>
+    {/if}
+</main>
+
+<style>
+@import '../app.css';
+</style>
